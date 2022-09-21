fbc7a0cdc8b7c0d1a85b84995eb196fd90dbe4b1
mac <chukwuekeuzoma@gmail.com>
2022-09-21 13:00:52 +0100

    updated the component

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,11 +2,13 @@ import Overview from "./Overview/Overview";
 import Login from "./Login/Login"
 import OverviewDetails from "./OverviewDetails/OverviewDetails"
 import StarShip from "./StarShip/StarShip"
+import People from "./People/People"
 
 
 export {
     OverviewDetails,
     Overview,
     Login,
-    StarShip
+    StarShip,
+    People
 }
