2968c7cd48079d775927a511cd6b30938b906e1d
mac <chukwuekeuzoma@gmail.com>
2022-09-21 11:10:49 +0100

    upadated the component

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,10 +1,12 @@
 import Overview from "./Overview/Overview";
 import Login from "./Login/Login"
 import OverviewDetails from "./OverviewDetails/OverviewDetails"
+import StarShip from "./StarShip/StarShip"
 
 
 export {
     OverviewDetails,
     Overview,
-    Login 
+    Login,
+    StarShip
 }
