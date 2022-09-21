2d9c4e20af8bfcb7023d0cd36f61b96a4717cd6f
mac <chukwuekeuzoma@gmail.com>
2022-09-21 11:10:43 +0100

    upadated the component

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -2,45 +2,50 @@ import React, { useState } from "react";
 import Starwars2 from "../../asset/Svg/Starwars2.svg";
 import OverviewIcon from "../../asset/Svg/OverviewIcon.svg";
 import "./SideBar.css";
+import { Link } from "react-router-dom";
 
 export default function SideBar(props) {
   const [active, setActive] = useState("");
- 
+
   return (
     <div className="DBN_container">
       <div className="DBN_options_container">
         <div className="DBN_Starwars">
           <img src={Starwars2} alt="logo" />
         </div>
-        <div className="DBN_icon_container">
-          <div
-            onClick={() => setActive("DashboardIcon")}
-            className={
-              active === "DashboardIcon"
-                ? "DBN_icon_container_select_active"
-                : "DBN_icon_container_select"
-            }
-          >
-            <img src={OverviewIcon} alt="logo" />
-            <span>
-              <span style={{ opacity: "0" }}>..</span>Overview
-            </span>
+        <Link to="/overview" className="links">
+          <div className="DBN_icon_container">
+            <div
+              onClick={() => setActive("DashboardIcon")}
+              className={
+                active === "DashboardIcon"
+                  ? "DBN_icon_container_select_active"
+                  : "DBN_icon_container_select"
+              }
+            >
+              <img src={OverviewIcon} alt="logo" />
+              <span>
+                <span style={{ opacity: "0" }}>..</span>Overview
+              </span>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="DBN_icon_container-two">
-          <div
-            onClick={() => setActive("Overview")}
-            className={
-              active === "Overview"
-                ? "DBN_icon_container_select_active"
-                : "DBN_icon_container_select"
-            }
-          >
-            <div className="box-icon"></div>
-            <span>
-              <span style={{ opacity: "0" }}>..</span>Starships
-            </span>
-          </div>
+          <Link to="/StarShip" className="links">
+            <div
+              onClick={() => setActive("Overview")}
+              className={
+                active === "Overview"
+                  ? "DBN_icon_container_select_active"
+                  : "DBN_icon_container_select"
+              }
+            >
+              <div className="box-icon"></div>
+              <span>
+                <span style={{ opacity: "0" }}>..</span>Starships
+              </span>
+            </div>
+          </Link>
           <div
             onClick={() => setActive("Starships")}
             className={
